a012ba3aacca55a0aa59b8f9a7836da7530ae9a7
Heike Winkelvoß <public@egladil.de>
2020-01-04 09:38:38 +0100

    solution nav with events

--- angular8-m-schwarzmueller/prj-basics-final/src/app/header/header.component.ts
+++ angular8-m-schwarzmueller/prj-basics-final/src/app/header/header.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output} from '@angular/core';
+import { RECIPES, SHOPPING_LIST } from '../shared/constants';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+
+  @Output() selectComponentEvent: EventEmitter<{component: string}> = new EventEmitter();
+
+
+  recipesSelected() {
+    this.selectComponentEvent.emit({component: RECIPES});
+  }
+
+  shoppingListSelected() {
+    this.selectComponentEvent.emit({component: SHOPPING_LIST});
+  }
 
 }
---
+++ angular8-m-schwarzmueller/prj-basics-final/src/app/shared/constants.ts
@@ -0,0 +1,4 @@
+
+export const RECIPES = 'recipes';
+export const SHOPPING_LIST = 'shopping list';
+
