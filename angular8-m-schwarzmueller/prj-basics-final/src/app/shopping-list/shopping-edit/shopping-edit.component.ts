4679bf577c69d7e48cd6304722633f5f4c14c128
Heike Winkelvoß <public@egladil.de>
2020-01-04 16:18:14 +0100

    add ingredient with events and ViewChild

--- angular8-m-schwarzmueller/prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ angular8-m-schwarzmueller/prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput', { static: false }) nameInput: ElementRef;
+
+  @ViewChild('amountInput', { static: false }) amountInput: ElementRef;
+
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  submit() {
+
+    const name = this.nameInput.nativeElement.value;
+    const amount = this.amountInput.nativeElement.value;
+
+    const ingredient = new Ingredient(name, amount);
+
+    this.ingredientAdded.emit(ingredient);
   }
 
 }
--- angular8-m-schwarzmueller/prj-basics-final/src/app/shopping-list/shopping-list.component.ts
+++ angular8-m-schwarzmueller/prj-basics-final/src/app/shopping-list/shopping-list.component.ts
@@ -18,4 +18,9 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  addIngredient(ingredient: Ingredient) {
+
+    this.ingredients.push(ingredient);
+  }
+
 }
