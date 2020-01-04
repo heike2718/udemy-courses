d232b0938216196d827af62053fe860540bbac8f
Heike Winkelvoß <public@egladil.de>
2020-01-04 10:31:29 +0100

    code refactored to item

--- angular8-m-schwarzmueller/prj-basics-final/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular8-m-schwarzmueller/prj-basics-final/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
